55f016db9fd6dcde16231315b9ccba9810cd741d
ankur <ankur.zilpelwar@gmail.com>
2020-09-26 00:39:39 +0530

    added logic for hi command

--- index.js
+++ index.js
@@ -5,12 +5,17 @@ const client = new Discord.Client();
 const prefix = "!";
 
 client.on("ready", () => {
-    console.log(`Logged in as ${client.user.tag}`)
+  console.log(`Logged in as ${client.user.tag}`);
 });
 
 client.on("message", (message) => {
   if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
+  if (!message.content.startsWith(prefix)) {
+    if (message.content === "hi") {
+      message.reply("Greetings!");
+    }
+    return;
+  }
   const commandBody = message.content.slice(prefix.length);
   const args = commandBody.split(" ");
   const command = args.shift().toLowerCase();
@@ -18,10 +23,11 @@ client.on("message", (message) => {
   if (command === "ping") {
     const timeTaken = Date.now() - message.createdTimestamp;
     message.reply(`Pong! This message had latency of ${timeTaken}ms.`);
-  } else if (command === "sum") {
-    const numArgs = args.map((x) => parseFloat(x));
-    const sum = numArgs.reduce((counter, x) => (counter += x));
-    message.reply(`The sum of all the arguments is ${sum}!`);
+  } else if (command === "google") {
+    //TODO User google custom serach api to search on google and return the first 5 links
+    //TODO store the recent searches, skip the duplicate search keywords
+  } else if (command === "recent") {
+    //TODO: search the heroku DB for replying the search keyword from the DB
   }
 });
 
